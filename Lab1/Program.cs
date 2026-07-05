958290543b0a59081bcb81b99cc8e4a174254f2a
Gk18038 <george18038@gmail.com>
2026-07-05 21:07:59 +0000

    Finished Lab 1

--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -1,7 +1,6 @@
-﻿namespace Lab1;
-class Program
+﻿internal class Program
 {
-    static void Main(string[] args)
+    private static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
         /* [George Koussa] [IT-1050 - Lab 1] */
@@ -14,13 +13,62 @@ class Program
         bool hasProgrammingExperience = false;
 
         Console.WriteLine(favoriteNumber);
-        Console.WriteLine("My favorite programming language is " + favoriteLanguage);
+        Console.WriteLine("My favorite programming language is: " + favoriteLanguage);
         Console.WriteLine("Programs written before this: " + programsWrittenBefore);
         Console.WriteLine("Programming experience: " + hasProgrammingExperience);
         // Step 4 
         const string schoolName = "School name is TRI-C";
         Console.WriteLine(schoolName);
         // Step 5
-        
+        double myDouble = 9.78;
+        int myInt = (int)myDouble;
+        int myInt2 = (int)myDouble;
+        bool myBool = true;
+        Console.WriteLine(Convert.ToString(myBool));
+        // Step 6
+        Console.WriteLine("Write your name here: ");
+        Console.WriteLine("Enter your age here: ");
+        Console.ReadLine();
+        int userAge = Convert.ToInt32(Console.ReadLine());
+        // Step 7
+        int num1 = 5;
+        int num2 = 10;
+
+        int additionResult = num1 + 10;
+        int subtractionResult = num1 - 2;
+        int multiplicationResult = num1 * 3;
+        int divisionResult = num1 / 2;
+        int modulusResult = num1 % 2;
+
+        Console.WriteLine("Addition result (num1 + 10): " + additionResult);
+        Console.WriteLine("Subtraction result (num1 - 2): " + subtractionResult);
+        Console.WriteLine("Multiplication result (num1 * 3): " + multiplicationResult);
+        Console.WriteLine("Division result (num1 / 2): " + divisionResult);
+        Console.WriteLine("Modulus result (num1 % 2): " + modulusResult);
+        int additionResult2 = num2 + 10;
+        int subtractionResult2 = num2 - 2;
+        int multiplicationResult2 = num2 * 3;
+        int divisionResult2 = num2 / 2;
+        int modulusResult2 = num2 % 2;
+
+        Console.WriteLine("Addition result (num2 + 10): " + additionResult2);
+        Console.WriteLine("Subtraction result (num2 - 2): " + subtractionResult2);
+        Console.WriteLine("Multiplication result (num2 * 3): " + multiplicationResult2);
+        Console.WriteLine("Division result (num2 / 2): " + divisionResult2);
+        Console.WriteLine("Modulus result (num2 % 2): " + modulusResult2);
+        // Step 8
+        float floatNum1 = 1.123456789f;
+        double doubleNum2 = 1.123456789d;
+        Console.WriteLine("Float number: " + floatNum1);
+        Console.WriteLine("Double number: " + doubleNum2);
+        // Step 9
+        int declared = 10;
+        Console.WriteLine("Initial value: " + declared);
+
+        declared++;
+        Console.WriteLine("After increment (++): " + declared);
+
+        declared--;
+        Console.WriteLine("After decrement (--): " + declared);
     }
 }
--- Lab1/obj/Debug/net10.0/Lab1.AssemblyInfo.cs
+++ Lab1/obj/Debug/net10.0/Lab1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+101e8c1e7fb2703b79fd8986eb4e0594d4334f14")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+05dd5d30658db86b0d8e78389530e31341e1a627")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
